9439a7f9c1cfeb6a3478889e9fd2baa9d244d947
SAMUELMUCHIRI <eng.njaumuchiri@gmail.com>
2022-07-12 15:24:59 +0300

    finally more sense
    Signed-off-by: SAMUELMUCHIRI <eng.njaumuchiri@gmail.com>

--- ROCK.py
+++ ROCK.py
@@ -5,17 +5,13 @@ def play():
     user=input("What is your choice'r' for rock,'p' for paper,'s'for scissors")
     computer= random.choice(['r','p','s'])
     print(computer)
-
     if user==computer:
-        return "tie"
-
-    if is_win(user, computer):
-        return "youwon"
-
-    return "you lost"
-
-def is_win(player, opponent):
-
-    if(player=='r' and opponent=='s') or (player=='s' and opponent=='p') or (player=='r' and opponent=='r'):
-        return True
+        print ("tie")
+    else :
+        def is_win():
+            if(user=='r' and computer=='s') or (user=='s' and computer=='p') or (user=='p' and computer=='r'):
+                print("you won")
+            else:
+                print("you lost")
+        is_win()
 play()
